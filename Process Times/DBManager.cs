b2e746d6e3037ce20d2143f77f1bf4244fb635bf
Pavel Zharkov <p.zharkov@hotmail.com>
2021-11-14 13:12:51 -0500

    Added test create DB function to DBManager.

---
+++ Process Times/DBManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Process_Times
+{
+    // This class is responsible for managing DB related functions.
+    class DBManager
+    {
+        public bool DBExists()
+        {
+            // Checks if file with the correct name exist and if DB structure matches target format (column number, names, and data types).
+            
+            // temp only return false to test
+            return false;
+        }
+
+        public void CreateDB()
+        {
+            // create db file
+        }
+
+        public void WriteToDB()
+        {
+            // write to db
+        }
+
+        public void ReadFromDB()
+        {
+            // read from db
+        }
+        
+    }
+}
