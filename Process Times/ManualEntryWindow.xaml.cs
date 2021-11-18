e216ee50a35701c7ad660de8e327aaaad3d07edd
Pavel Zharkov <p.zharkov@hotmail.com>
2021-11-18 18:23:18 -0500

    Added Manual Entry and Generate Data Set windows. Created Manual Entry UI.

---
+++ Process Times/ManualEntryWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Process_Times
+{
+    /// <summary>
+    /// Interaction logic for ManualEntryWindow.xaml
+    /// </summary>
+    public partial class ManualEntryWindow : Window
+    {
+        public ManualEntryWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
